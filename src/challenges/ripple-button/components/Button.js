c053e1798092865edd52843904c293804b1f79cd
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-02-26 16:55:53 +0200

    some sort of mouse follow behaviour

--- src/challenges/ripple-button/components/Button.js
+++ src/challenges/ripple-button/components/Button.js
@@ -1,12 +1,28 @@
+import { useState, useRef } from 'react';
+
 const Button = () => {
+  const [effect, setEffect] = useState(false);
+  const mousePosition = useRef({ x: 0, y: 0 });
+
   const handleMousePosition = (e) => {
-    // const button = document.getElementsByClassName('ripple-button');
+    const button = document.getElementById('mouse-trail');
+    button.style.display = 'block';
+    setEffect(!effect);
+    if (effect) {
+      console.log('jorma');
+    }
     console.log(e.pageY);
+    console.log(e.pageX);
+    mousePosition.current.x = e.pageX;
+    mousePosition.current.y = e.pageY;
+    button.style.left = e.pageX + 'px';
+    button.style.top = e.pageY + 'px';
   };
 
   return (
     <div className="ripple-button-container">
-      <button type="button" className="ripple-button" onMouseMove={(e) => handleMousePosition(e)}>Rippling</button>
+      <button type="button" className="ripple-button" onMouseMove={(e) => handleMousePosition(e)} onClick={() => console.log('jorma')}>Rippling</button>
+      <div id="mouse-trail"></div>
     </div>
   );
 };
