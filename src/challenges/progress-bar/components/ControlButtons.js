2984f0525e0637cc53487cab364bb81fbca3dcc5
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2023-03-01 16:41:31 +0200

    button logic

--- src/challenges/progress-bar/components/ControlButtons.js
+++ src/challenges/progress-bar/components/ControlButtons.js
@@ -1,9 +1,35 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+
 const ControlButtons = () => {
+  const [intervalGoing, setIntervalGoing] = useState(false);
+  const [incrementWidth, setIncrementWidth] = useState(0);
+  const [timer, setTimer] = useState(null);
+  console.log(incrementWidth);
+  useEffect(() => {
+    if (intervalGoing) {
+      setTimer(setInterval(() => {
+        setIncrementWidth(incrementWidth + 1);
+      }, 1000));
+    }
+    return () => clearInterval(timer);
+  }, [incrementWidth, intervalGoing]);
+
+  const handleStop = () => {
+    setIntervalGoing(false);
+    clearInterval(timer);
+  };
+
+  const handleReset = () => {
+    setIncrementWidth(0);
+    clearInterval(timer);
+  };
+
   return (
     <div className="control-button-container">
-      <button className="control-button">Start</button>
-      <button className="control-button">Stop</button>
-      <button className="control-button">Retry</button>
+      <button className="control-button" onClick={() => setIntervalGoing(true)}>Start</button>
+      <button className="control-button" onClick={() => handleStop()}>Stop</button>
+      <button className="control-button" onClick={() => handleReset()}>Reset</button>
     </div>
   );
 };
